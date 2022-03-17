75c65f91a8d6d06a205bd3064a00b7cd0231d42d
solar <spidervvv@mail.ru>
2022-03-17 21:34:46 +0300

    imlementing error and success messages

--- src/js/controller.js
+++ src/js/controller.js
@@ -20,7 +20,7 @@ const controlRecipes = async function () {
     //!Rendering recipe
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
