7d0cafc12124173d33d86dce257b2a4fb8ec16a2
solar <spidervvv@mail.ru>
2022-03-22 21:39:24 +0300

    fix page by default for new search

--- src/js/model.js
+++ src/js/model.js
@@ -37,6 +37,9 @@ export const loadSearchResults = async function (query) {
   try {
     state.search.query = query;
 
+    // Reset page by default for new search
+    state.search.page = 1;
+
     const data = await getJSON(`${API_URL}?search=${query}`);
 
     state.search.results = data.data.recipes.map(rec => {
